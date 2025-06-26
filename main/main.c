c5d1230485c6621f9d84f16adb2ce8bf91a2e17f
Aleksandr <sanchous.vas@gmail.com>
2025-06-26 17:08:05 +0300

    add ld blink example

--- main/main.c
+++ main/main.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
+#include "esp_err.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
 #include "sf8xxx_nm.h"
@@ -13,11 +15,21 @@ static const char *TAG = "ST8XXX_NM main";
 
 #define DELAY_BTW_CMDS vTaskDelay(pdMS_TO_TICKS(150))
 
+void print_driver_state(sf8xxx_nm_driver_state_info_t driver_state) {
+    ESP_LOGI(TAG, "--- Состояние драйвера ---");
+    ESP_LOGI(TAG, "Драйвер ВКЛ: %s", driver_state.is_powered_on ? "ДА" : "НЕТ");
+    ESP_LOGI(TAG, "Драйвер ЗАПУЩЕН: %s", driver_state.is_started ? "ДА" : "НЕТ");
+    ESP_LOGI(TAG, "Ток установлен внутренним: %s", driver_state.current_set_internal ? "ДА" : "НЕТ");
+    ESP_LOGI(TAG, "Включение внутреннее: %s", driver_state.enable_internal ? "ДА" : "НЕТ");
+    ESP_LOGI(TAG, "Интерлок внешнего NTC отклонен: %s", driver_state.ext_ntc_interlock_denied ? "ДА" : "НЕТ");
+    ESP_LOGI(TAG, "Интерлок отклонен: %s", driver_state.interlock_denied ? "ДА" : "НЕТ");
+}
+
 void ST8XXX_NM_task(void *pvParams) {
     sf8xxx_nm_err_t err = SF8XXX_NM_OK;
     sf8xxx_nm_init();
-    gpio_pulldown_en(CONFIG_SF8XXX_NM_UART_RXD);
-    gpio_pulldown_en(CONFIG_SF8XXX_NM_UART_TXD);
+    ESP_ERROR_CHECK(gpio_pulldown_en(CONFIG_SF8XXX_NM_UART_RXD));
+    ESP_ERROR_CHECK(gpio_pulldown_en(CONFIG_SF8XXX_NM_UART_TXD));
 
     uint16_t serial_number, device_id;
     err = sf8xxx_nm_get_serial_number(&serial_number);
@@ -30,27 +42,113 @@ void ST8XXX_NM_task(void *pvParams) {
     ESP_LOGI(TAG, "Serial number: %d", serial_number);
     ESP_LOGI(TAG, "Device id: %d", device_id);
 
+    float frequency_hz, duration_ms, current_val, tec_temp;
+    err = sf8xxx_nm_set_freq(0.0);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка установки частоты.");
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_get_freq(&frequency_hz);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка чтения частоты.");
+    } else {
+        ESP_LOGI(TAG, "Установленная частота испульса: %.1f Hz.", frequency_hz);
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_set_duration(6.9);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка установки продолжительности импульса.");
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_get_duration(&duration_ms);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка чтения продолжительности импульса.");
+    } else {
+        ESP_LOGI(TAG, "Установленная продолжительность испульса: %.1f ms.", duration_ms);
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_set_current(100.0);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка установки тока на ЛД.");
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_get_current(&current_val);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка чтения тока на ЛД.");
+    } else {
+        ESP_LOGI(TAG, "Установленный ток на ЛД: %.1f mA.", current_val);
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_set_tec_temp(20.0);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка установки температуры на TEC.");
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_get_tec_temp(&tec_temp);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка чтения температуры на TEC.");
+    } else {
+        ESP_LOGI(TAG, "Установленная температура на TEC: %.1f C.", current_val);
+    }
+    DELAY_BTW_CMDS;
+
+    
+    sf8xxx_nm_driver_state_info_t driver_state;
+    err = sf8xxx_nm_get_driver_state(&driver_state);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка чтения состояния драйвера.");
+    } else {
+        ESP_LOGI(TAG, "Начальное состояние драйвера:");
+        print_driver_state(driver_state);
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_set_driver_state(SF8XXX_NM_DRIVER_STATE_WRITE_INTERNAL_CURRENT_SET);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка установки состояния драйвера.");
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_set_driver_state(SF8XXX_NM_DRIVER_STATE_WRITE_INTERNAL_ENABLE);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка установки состояния драйвера.");
+    }
+    DELAY_BTW_CMDS;
+
+    err = sf8xxx_nm_set_driver_state(SF8XXX_NM_DRIVER_STATE_WRITE_DENY_INTERLOCK);
+    if (err != SF8XXX_NM_OK) {
+        ESP_LOGE(TAG, "Ошибка установки состояния драйвера.");
+    }
+    DELAY_BTW_CMDS;
+
     while (1) {
-        float frequency_hz;
-        ESP_LOGI(TAG, "Set frequency 0.0 Hz");
-        sf8xxx_nm_set_freq(0.0);
-
+        err = sf8xxx_nm_set_driver_state(SF8XXX_NM_DRIVER_STATE_WRITE_START);
+        if (err != SF8XXX_NM_OK) {
+            ESP_LOGE(TAG, "Ошибка включения ЛД.");
+        } else {
+            ESP_LOGI(TAG, "ЛД вкл.");
+        }
         DELAY_BTW_CMDS;
+        
+        vTaskDelay(pdMS_TO_TICKS(2000));
 
-        sf8xxx_nm_get_freq(&frequency_hz);
-        ESP_LOGI(TAG, "Measured Frequency: %.1f Hz", frequency_hz);
 
+        err = sf8xxx_nm_set_driver_state(SF8XXX_NM_DRIVER_STATE_WRITE_STOP);
+        if (err != SF8XXX_NM_OK) {
+            ESP_LOGE(TAG, "Ошибка выключения ЛД.");
+        } else {
+            ESP_LOGI(TAG, "ЛД выкл.");
+        }
         DELAY_BTW_CMDS;
-
-        ESP_LOGI(TAG, "Set frequency 10.2 Hz");
-        sf8xxx_nm_set_freq(10.2);
-
-        DELAY_BTW_CMDS;
-
-        sf8xxx_nm_get_freq(&frequency_hz);
-        ESP_LOGI(TAG, "Measured Frequency: %.1f Hz", frequency_hz);
-
-        vTaskDelay(pdMS_TO_TICKS(1500));
+        
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 
     // never reach here
